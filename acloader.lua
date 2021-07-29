c4e955d93db183116cb8bbf5ccf79ee7db6f0c02
GamesLigaTv <38510802+GamesLigaTv@users.noreply.github.com>
2021-07-29 18:39:02 +0200

    Update acloader.lua

--- acloader.lua
+++ acloader.lua
@@ -1,13 +1,13 @@
-local ProhibitedVariables = { -- Add as many as you want from mod menus you find! (Remove war menu if you use it!)
+local ProhibitedVariables = { -- Füge so viele wie du willst aus den Mod-Menüs hinzu, die du findest! (Entferne das Kriegsmenü, wenn du es verwendest!) 
 "fESX", "Plane", "TiagoMenu", "Outcasts666", "dexMenu", "Cience", "LynxEvo", "zzzt", "AKTeam",
 "gaybuild", "ariesMenu", "WarMenu", "SwagMenu", "Dopamine", "Gatekeeper", "MIOddhwuie"
 }
-local Enabled = false -- Change this to enable client mod menu checks!
+local Enabled = false -- Ändere dies, um Client-Mod-Menüprüfungen zu aktivieren! 
 function CheckVariables()
     for i, v in pairs(ProhibitedVariables) do
         if _G[v] ~= nil then
             local reason = Config.Messages.ProhibitedVariables:gsub("{VARIABLE}", v);
-            TriggerServerEvent('Anticheat:Modder', "[Badger-Anticheat]: " .. reason);
+            TriggerServerEvent('Anticheat:Modder', "[FastCity-Anticheat]: " .. reason);
         end
     end
 end
